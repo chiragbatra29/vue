ccda67fa7f48633d23c699b473e61b4c6f44621a
chiragbatra29 <chiragbatra1994@gmail.com>
2017-06-21 15:39:40 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<!-- <template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
+<!-- <script src="https://unpkg.com/vue"></script> -->
+<template>
+  <div id="app">
+    <span v-bind:title="message">
+      Hover your mouse over me for a few seconds
+      to see my dynamically bound title!
+    </span>
+  </div>
+</template>
